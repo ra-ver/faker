7287b03c13d2b96a1698b83d2ce2daa1c741f590
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-01-09 11:33:31 -0500

    Fix for 'Too Many Requests' Issue and Schema changes

--- src/export-data.js
+++ src/export-data.js
@@ -45,13 +45,16 @@ export default class DataExporter {
         // writing one by one to avoid hitting payload limit
         data.forEach(async (item, index) => {
             try {
-                await setTimeout(() => {}, 2000);
-                let response = await axios.post(`http://localhost:9200/docs/models`, item).then(x => new Promise(resolve => setTimeout(() => resolve(x), 4000)));
-                await setTimeout(() => {}, 2000);
-                this.logger.info("Exported " + index + " to Elastic Search.")
+                await this.sleep(index*200*Math.random());
+                let response = await axios.post(`http://142.1.177.54:9200/docs/models`, item);
+                this.logger.info("Exported " + index + " to Elastic Search.");
             } catch (err) {
+                this.logger.info("Error exporting " + index + " to Elastic Search.");
                 this.logger.error(err);
             }
         });
+    }
+    async sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
